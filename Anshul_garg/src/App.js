2ebe62aee23a71a2be717b46bfd84642ae5545a0
Anshul Garg <anshulgarg1133@gmail.com>
2026-07-21 21:57:42 +0530

    Fix: added correct todolist file

--- Anshul_garg/src/App.js
+++ Anshul_garg/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todolist, setTodolist]= useState([])
+
+   let SaveTask=(event)=>{
+    
+    let task=event.target.task.value;
+    if (!todolist.includes(task)){
+      let finalList=[...todolist,task]
+      setTodolist(finalList)
+    }
+    else{
+      alert("Task already exists...")
+    }
+    
+    event.preventDefault();
+  }
+  let list=todolist.map((value,index)=>{
+    return(
+      <TodoListItems value={value} key={index} indexNumber={index} 
+      todolist={todolist} 
+      setTodolist={setTodolist}
+      />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <h1>ToDo List</h1>
+     <form onSubmit={SaveTask}>
+      <input type='text' name="task"/> <button>Add</button>
+     </form>
+
+     <div className='Lists'>
+       <ul>
+         {list}
+       </ul>
+     </div>
     </div>
   );
 }
 
 export default App;
+
+function TodoListItems({value,indexNumber,todolist,setTodolist}){
+  let [status,setStatus]= useState(false)
+
+  let deleteRow=()=>{
+    let final=todolist.filter((v,i)=>i!=indexNumber)
+    setTodolist(final)
+  }
+  let checkStatus=()=>{
+    setStatus(!status)
+  }
+  return(
+    <li className={(status)? 'completeTask':''} onClick={checkStatus}>{indexNumber+1}.  {value}<span onClick={deleteRow}>&times;</span></li>
+  )
+}
+
